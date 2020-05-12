4b0e903935fd01ea64ec05ee8c6fbd3e96a5e7e0
Sookhee <s2018s38@e-mirim.hs.kr>
2020-05-12 11:28:39 +0900

    실습일지 2번

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -19,44 +19,51 @@ namespace csloop
             //}
             //Console.WriteLine(count + "만큼 반복했습니다");
 
-            string[] array = { "사과", "배", "포도", "딸기", "바나나" };
-            foreach (string item in array)
+            //string[] array = { "사과", "배", "포도", "딸기", "바나나" };
+            //foreach (string item in array)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //string input = "Potato Tomato";
+            //Console.WriteLine(input.ToUpper());
+            //Console.WriteLine(input.ToLower());
+
+            //input = "감자 고구마 토마토";
+            //string[] inputs = input.Split(new Char[] { ' ' });
+            //foreach (var item in inputs)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //string[] arr = { "감자", "고구마", "토마토", "가지" };
+            //Console.WriteLine(string.Join(", ", arr));
+
+            //int x = 1;
+            //while(x < 50)
+            //{
+            //    //화면 지우고 커서 이동
+            //    Console.Clear();
+            //    Console.SetCursorPosition(x, 5);
+
+            //    //출력
+            //    if (x % 3 == 0)
+            //        Console.WriteLine("__@");
+            //    else if (x % 3 == 1)
+            //        Console.WriteLine("_^@");
+            //    else
+            //        Console.WriteLine("^_@");
+
+            //    //100밀리초 정지하고 x를 증가합니다.
+            //    Thread.Sleep(100);
+            //    x++;    
+            //}
+
+            // 실습일지 2번
+            int[] array = new int[10];
+            foreach(var item in array)
             {
                 Console.WriteLine(item);
-            }
-
-            string input = "Potato Tomato";
-            Console.WriteLine(input.ToUpper());
-            Console.WriteLine(input.ToLower());
-
-            input = "감자 고구마 토마토";
-            string[] inputs = input.Split(new Char[] { ' ' });
-            foreach (var item in inputs)
-            {
-                Console.WriteLine(item);
-            }
-
-            string[] arr = { "감자", "고구마", "토마토", "가지" };
-            Console.WriteLine(string.Join(", ", arr));
-
-            int x = 1;
-            while(x < 50)
-            {
-                //화면 지우고 커서 이동
-                Console.Clear();
-                Console.SetCursorPosition(x, 5);
-
-                //출력
-                if (x % 3 == 0)
-                    Console.WriteLine("__@");
-                else if (x % 3 == 1)
-                    Console.WriteLine("_^@");
-                else
-                    Console.WriteLine("^_@");
-
-                //100밀리초 정지하고 x를 증가합니다.
-                Thread.Sleep(100);
-                x++;    
             }
 
         }
